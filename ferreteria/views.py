63cc6466f6b2d77217946f0f75e26966323d9bb4
Benjamin Rustrian <jrustrianl@miumg.edu.gt>
2023-09-18 21:55:19 -0600

    azure storage media

--- ferreteria/views.py
+++ ferreteria/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.hashers import make_password, check_password
 
 from django.db import IntegrityError, DataError
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 
 from rest_framework import status
 from rest_framework import viewsets
@@ -26,7 +26,7 @@ class ClienteViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def create(self, request, pk=None):
-        if 'nombre' in request.data and 'email' in request.data and 'password' in request.data and 'telefono' in request.data and 'fecha_nacimiento':
+        if 'nombre' in request.data and 'email' in request.data and 'password' in request.data and 'telefono' in request.data and 'fecha_nacimiento' in request.data:
             hashed_password = make_password(request.data['password'])
             try:
                 new_cliente = Cliente.objects.create(nombre=request.data['nombre'], email=request.data['email'], password = hashed_password, telefono=request.data['telefono'], fecha_nacimiento=request.data['fecha_nacimiento'])
@@ -36,8 +36,74 @@ class ClienteViewSet(viewsets.ViewSet):
             except DataError as data_error:
                 content = {'message': str(data_error)}
                 return Response(content, status=status.HTTP_404_NOT_FOUND)
+            except ValidationError as validation_error:
+                content = {'message': str(validation_error)}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
 
             cliente_serializer = ClienteSerializer(new_cliente, many=False)
+            return Response(cliente_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            content = {'message': 'Datos incompletos'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        if 'nombre' in request.data and 'telefono' in request.data and 'fecha_nacimiento' in request.data:
+            aux_cliente = None
+            try:
+                aux_cliente = Cliente.objects.get(pk=pk)
+            except ObjectDoesNotExist:
+                content = {'message': 'No existe cliente'}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+            try:
+                aux_cliente.nombre = request.data['nombre']
+                aux_cliente.telefono = request.data['telefono']
+                aux_cliente.fecha_nacimiento = request.data['fecha_nacimiento']
+                aux_cliente.save()
+            except IntegrityError as integrity_error:
+                content = {'message': str(integrity_error)}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+            except DataError as data_error:
+                content = {'message': str(data_error)}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+            except ValidationError as validation_error:
+                content = {'message': str(validation_error)}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+            cliente_serializer = ClienteSerializer(aux_cliente, many=False)
+            return Response(cliente_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            content = {'message': 'Datos incompletos'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    
+    def partial_update(self, request, pk=None):
+        if 'old_password' in request.data and 'password' in request.data:
+            aux_cliente = None
+            try:
+                aux_cliente = Cliente.objects.get(pk=pk)
+            except ObjectDoesNotExist:
+                content = {'message': 'No existe cliente'}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+            
+            if not check_password(request.data['old_password'], aux_cliente.password):
+                content = {'message': 'Contraseña actual no es correcta'}
+                return Response(content, status=status.HTTP_403_FORBIDDEN)
+
+            try:
+                hashed_password = make_password(request.data['password'])
+                aux_cliente.password = hashed_password
+                aux_cliente.save()
+            except IntegrityError as integrity_error:
+                content = {'message': str(integrity_error)}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+            except DataError as data_error:
+                content = {'message': str(data_error)}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+            except ValidationError as validation_error:
+                content = {'message': str(validation_error)}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+            cliente_serializer = ClienteSerializer(aux_cliente, many=False)
             return Response(cliente_serializer.data, status=status.HTTP_201_CREATED)
         else:
             content = {'message': 'Datos incompletos'}
--- ferreteria_backend/settings.py
+++ ferreteria_backend/settings.py
@@ -125,9 +125,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.azure_storage.AzureStorage",
+        "OPTIONS":{
+            "account_name": "ferreteriaimg",
+            "account_key": "x8UK4HAmpDIqxXujd6RwQNujYMep9Ou6+6WoaRkI1Fiq4T365KGUdeHjtqyyr2YFDQA/FnV0qekp+ASth0zIEg==",
+            "custom_domain": "ferreteriaimg.blob.core.windows.net",
+            "azure_container": "mediaimages"
+        }
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#MEDIA_URL = 'media/'
+#MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
